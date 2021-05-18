c2016304796a7ccd598dc626c63365f6e1462d02
ynoTL <tonylu23@gamil.com>
2021-05-18 16:53:00 -0400

    design and implement homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css'
+import Home from './components/Home'
 
 function App() {
   return (
     <div className='App'>
-      <h1>This is my portfolio</h1>
+      <div className='bounds-container'>
+        <div className='line top'></div>
+        <div className='line right'></div>
+        <div className='line bottom'></div>
+        <div className='line left'></div>
+      </div>
+
+      <div className='wrapper'>
+        <div className='home-container'>
+          <Home />
+        </div>
+      </div>
     </div>
   )
 }
